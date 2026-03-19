8b51b35e3683d6299ffd62f661fa1726ce37d8a1
Jacob Johnson <jakeymacaroni@gmail.com>
2026-03-19 00:26:34 -0600

    * Updated settings.py file * Updated docker-compose file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True")
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "18.191.192.9"]
 
