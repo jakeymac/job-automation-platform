31976e71190c226c5db5979e75090bc8cd034efd
Jacob Johnson <jakeymacaroni@gmail.com>
2026-03-31 01:03:42 -0600

    Added logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
     <header className="header">
       <div className="header-left">
         <Link to="/" className="logo">
+          <img src="/icon.png" alt="Logo" className="logo-icon" />
           Automated Jobs
         </Link>
 
